613ac864b696732200a02647c64c936de0dddcc6
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-07-17 20:07:57 +0200

    Adding platformio

--- arduino/src/main/main.ino
+++ arduino/src/main/main.ino
@@ -1,23 +1,56 @@
 #include "pinout.h"
+#include "status.pb.h"
+#include <Servo.h>
+
+Servo leftMotor;
+Servo rightMotor;
 
 void setup_pin(){
-  pinMode(MOTOR_PWM_RIGHT, OUTPUT);
-  pinMode(MOTOR_PWM_LEFT,  OUTPUT);
-  pinMode(MOTOR_BREAK,     OUTPUT);
 
+  pinMode(MOTOR_BREAK,      OUTPUT);
   digitalWrite(MOTOR_BREAK, LOW);
+
+  leftMotor.attach(MOTOR_PWM_LEFT);
+  leftMotor.write(90);
+
+  rightMotor.attach(MOTOR_PWM_RIGHT);
+  rightMotor.write(90);
 }
 
-
-
 void setup() {
-  Serial.begin(9600);
   setup_pin();
+  Serial3.begin(9600);
+  Serial3.println("Begin");
 }
 
 void loop() {
-  digitalWrite(MOTOR_BREAK, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(MOTOR_BREAK, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  digitalWrite(MOTOR_BREAK, HIGH);
+  delay(100);
+  Serial3.println("Hallo");
+
+/*
+
+  int pos = 0;    // variable to store the servo position
+  
+  for (pos = 90; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    rightMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(50);                       // waits 15ms for the servo to reach the position
+  }
+  
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    leftMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    rightMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(50);                       // waits 15ms for the servo to reach the position
+  }
+
+
+  for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    leftMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    rightMotor.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(50);                       // waits 15ms for the servo to reach the position
+  }
+*/
 }
