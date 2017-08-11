14740ca15d20116390996b7a8f3e8f7f1f7bf804
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-11 14:33:58 +0200

    More debug

--- sisyphus/python/mcp.py
+++ sisyphus/python/mcp.py
@@ -1,33 +1,38 @@
 import serial
+import time
 import config_pb2
 import motor_pb2
 import sensor_pb2
 import status_pb2
-from struct import *
+from   struct     import *
 
 state = 0
-# 0 - wait
-# 1 - A 0x41
-# 2 - N 0x4E
-# 3 - S 0x53
-# 4 - I 0x49
-# 5 - SIZE
-# 6 - SIZE
+# 0     -> wait
+# 1     -> A 0x41
+# 2     -> N 0x4E
+# 3     -> S 0x53
+# 4     -> I 0x49
+# 5     -> SIZE
+# 6     -> SIZE
+# 7-end -> DATA
 
-with serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=100) as ser:
+with serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=1000) as ser:
 
-    hexsize = ''
-    buffer = ''
+    hexsize       = ''
+    buffer        = ''
     messagelength = 6
 
     while state < messagelength:
 
         c = ser.read()
 
-        if state == 0 and ord(c) == 0x41:
-            state = 1
-            print "A"
-            continue
+        if state == 0:
+            if ord(c) == 0x41:
+                state = 1
+                print "A"
+                continue
+            else:
+                state = 0
 
         if state == 1:
             if ord(c) == 0x4E:
@@ -65,15 +70,15 @@ with serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=100) as ser:
             state = 6
             buffer = ''
             messagelength += unpack('<H', hexsize)[0]
-            print "size"
+            print "size:%d" % (messagelength-6)
             continue
 
         if state > 5:
             state += 1
             buffer += c
 
-    print len(buffer)
-
+    print "Buffersize:%d" % len(buffer)
+    time.sleep(0.1)
     status = status_pb2.Status()
     status.ParseFromString(buffer)
     print status
