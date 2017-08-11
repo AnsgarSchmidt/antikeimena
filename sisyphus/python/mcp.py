c322c5bf7074de2194d02035f224d461c5e2ca3f
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-11 19:59:39 +0200

    Analyse more then one data package

--- sisyphus/python/mcp.py
+++ sisyphus/python/mcp.py
@@ -6,81 +6,113 @@ import sensor_pb2
 import status_pb2
 from   struct     import *
 
-state = 0
 # 0     -> wait
 # 1     -> A 0x41
 # 2     -> N 0x4E
 # 3     -> S 0x53
 # 4     -> I 0x49
-# 5     -> SIZE
+# 5     -> MESSAGE TYPE
 # 6     -> SIZE
-# 7-end -> DATA
+# 7     -> SIZE
+# 8-end -> DATA
 
-with serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=1000) as ser:
 
-    hexsize       = ''
-    buffer        = ''
-    messagelength = 6
+def debugData():
+    with serial.Serial('/dev/cu.wchusbserial1410', 9600, timeout=10000) as ser:
+        state         = 0
+        hexsize       = ''
+        buffer        = ''
+        messagelength = 7
+        messagetype   = 0
 
-    while state < messagelength:
+        while state < messagelength:
 
-        c = ser.read()
+            c = ser.read()
 
-        if state == 0:
-            if ord(c) == 0x41:
-                state = 1
-                print "A"
+            if state == 0:
+                if ord(c) == 0x41:
+                    state += 1
+                    print "A"
+                    continue
+                else:
+                    state = 0
+
+            if state == 1:
+                if ord(c) == 0x4E:
+                    state += 1
+                    print "N"
+                    continue
+                else:
+                    state = 0
+
+            if state == 2:
+                if ord(c) == 0x53:
+                    state += 1
+                    print "S"
+                    continue
+                else:
+                    state = 0
+
+            if state == 3:
+                if ord(c) == 0x49:
+                    state += 1
+                    print "I"
+                    hexsize = ''
+                    continue
+                else:
+                    state = 0
+
+            if state == 4:
+                messagetype = ord(c)
+                state += 1
+                print "MessageType: %d" % messagetype
                 continue
-            else:
-                state = 0
 
-        if state == 1:
-            if ord(c) == 0x4E:
-                state = 2
-                print "N"
+            if state == 5:
+                hexsize += c
+                state += 1
+                print "size"
                 continue
-            else:
-                state = 0
 
-        if state == 2:
-            if ord(c) == 0x53:
-                state = 3
-                print "S"
+            if state == 6:
+                hexsize += c
+                state += 1
+                buffer = ''
+                messagelength += unpack('<H', hexsize)[0]
+                print "size:%d" % (messagelength-7)
                 continue
-            else:
-                state = 0
 
-        if state == 3:
-            if ord(c) == 0x49:
-                state = 4
-                print "I"
-                hexsize = ''
-                continue
-            else:
-                state = 0
+            if state > 6:
+                state += 1
+                buffer += c
 
-        if state == 4:
-            hexsize += c
-            state = 5
-            print "size"
-            continue
+        print "Buffersize:%d" % len(buffer)
+        time.sleep(0.1)
 
-        if state == 5:
-            hexsize += c
-            state = 6
-            buffer = ''
-            messagelength += unpack('<H', hexsize)[0]
-            print "size:%d" % (messagelength-6)
-            continue
+        if messagetype == 4:
+            print "STATUS:"
+            status = status_pb2.Status()
+            status.ParseFromString(buffer)
+            print status
 
-        if state > 5:
-            state += 1
-            buffer += c
+        if messagetype == 3:
+            print "Sensor:"
+            sensor = sensor_pb2.Sensor()
+            sensor.ParseFromString(buffer)
+            print sensor
 
-    print "Buffersize:%d" % len(buffer)
-    time.sleep(0.1)
-    status = status_pb2.Status()
-    status.ParseFromString(buffer)
-    print status
+        if messagetype == 2:
+            print "Motor:"
+            motor = motor_pb2.Motor()
+            motor.ParseFromString(buffer)
+            print motor
 
+        if messagetype == 1:
+            print "Config:"
+            config = config_pb2.Config()
+            config.ParseFromString(buffer)
+            print config
 
+if __name__ == "__main__":
+    for i in range(100):
+        debugData()
