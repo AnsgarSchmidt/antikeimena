b48ac5fc3a8e2b4f4a3087f352c5ddd4cdd3715f
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-16 22:58:28 +0200

    Do NOT close the connection each time

--- sisyphus/python/mcp.py
+++ sisyphus/python/mcp.py
@@ -16,95 +16,95 @@ from   struct     import *
 # 8-end -> DATA
 
 
-def debugData():
-    with serial.Serial('/dev/cu.wchusbserial1410', 115200, timeout=10000) as ser:
-        state         = 0
-        hexsize       = ''
-        messagetype   = 0
-        messagelength = 0
+def debugData(ser):
+    state         = 0
+    hexsize       = ''
+    messagetype   = 0
+    messagelength = 0
 
-        while state < 7:
+    while state < 7:
 
-            c = ser.read()
+        c = ser.read()
 
-            if state == 0:
-                if ord(c) == 0x41:
-                    state += 1
-                    print ("A")
-                    continue
-                else:
-                    state = 0
-
-            if state == 1:
-                if ord(c) == 0x4E:
-                    state += 1
-                    print ("N")
-                    continue
-                else:
-                    state = 0
-
-            if state == 2:
-                if ord(c) == 0x53:
-                    state += 1
-                    print ("S")
-                    continue
-                else:
-                    state = 0
-
-            if state == 3:
-                if ord(c) == 0x49:
-                    state += 1
-                    print ("I")
-                    continue
-                else:
-                    state = 0
-
-            if state == 4:
-                messagetype = ord(c)
+        if state == 0:
+            if ord(c) == 0x41:
                 state += 1
-                print ("MessageType: %d" % messagetype)
+                print ("A")
                 continue
+            else:
+                state = 0
 
-            if state == 5:
-                hexsize = c
+        if state == 1:
+            if ord(c) == 0x4E:
                 state += 1
-                print ("size")
+                print ("N")
                 continue
+            else:
+                state = 0
 
-            if state == 6:
-                hexsize += c
+        if state == 2:
+            if ord(c) == 0x53:
                 state += 1
-                messagelength = unpack('<H', hexsize)[0]
-                print ("size:%d" % messagelength)
+                print ("S")
                 continue
+            else:
+                state = 0
 
-        message = ser.read(messagelength)
-        print ("Buffersize:%d" % len(message))
+        if state == 3:
+            if ord(c) == 0x49:
+                state += 1
+                print ("I")
+                continue
+            else:
+                state = 0
 
-        if messagetype == 4:
-            print ("STATUS:")
-            status = status_pb2.Status()
-            status.ParseFromString(message)
-            print (status)
+        if state == 4:
+            messagetype = ord(c)
+            state += 1
+            print ("MessageType: %d" % messagetype)
+            continue
 
-        if messagetype == 3:
-            print ("Sensor:")
-            sensor = sensor_pb2.Sensor()
-            sensor.ParseFromString(message)
-            print (sensor)
+        if state == 5:
+            hexsize = c
+            state += 1
+            print ("size")
+            continue
 
-        if messagetype == 2:
-            print ("Motor:")
-            motor = motor_pb2.Motor()
-            motor.ParseFromString(message)
-            print (motor)
+        if state == 6:
+            hexsize += c
+            state += 1
+            messagelength = unpack('<H', hexsize)[0]
+            print ("size:%d" % messagelength)
+            continue
 
-        if messagetype == 1:
-            print ("Config:")
-            config = config_pb2.Config()
-            config.ParseFromString(message)
-            print (config)
+    message = ser.read(messagelength)
+    print ("Buffersize:%d" % len(message))
+
+    if messagetype == 4:
+        print ("STATUS:")
+        status = status_pb2.Status()
+        status.ParseFromString(message)
+        print (status)
+
+    if messagetype == 3:
+        print ("Sensor:")
+        sensor = sensor_pb2.Sensor()
+        sensor.ParseFromString(message)
+        print (sensor)
+
+    if messagetype == 2:
+        print ("Motor:")
+        motor = motor_pb2.Motor()
+        motor.ParseFromString(message)
+        print (motor)
+
+    if messagetype == 1:
+        print ("Config:")
+        config = config_pb2.Config()
+        config.ParseFromString(message)
+        print (config)
 
 if __name__ == "__main__":
-    for i in range(23):
-        debugData()
+    with serial.Serial('/dev/cu.wchusbserial1410', 115200, timeout=10000) as ser:
+        for i in range(23):
+            debugData(ser)
