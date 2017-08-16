293b3600ce0a80968936eca18ec3026758d45d3e
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-16 21:52:50 +0200

    shorter delay

--- arduino/src/main.h
+++ arduino/src/main.h
@@ -9,7 +9,8 @@ extern "C" {
     #include "status.pb.h"
 }
 
-#define SCHEDULER_SEND_STATUS_DELAY 10000
-#define SCHEDULER_SEND_SENSOR_DELAY  1000
+#define SCHEDULER_SEND_STATUS_DELAY      10000
+#define SCHEDULER_SEND_SENSOR_DELAY        100
+#define SCHEDULER_MEASURE_ULTRASONIC_DELAY  10
 
 #endif
