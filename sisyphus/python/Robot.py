9a56de6a221c4e76c8a9836c6e093fd42912b4d5
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2017-08-20 18:36:24 +0200

    debug

--- sisyphus/python/Robot.py
+++ sisyphus/python/Robot.py
@@ -98,7 +98,7 @@ class ReceiveThread(threading.Thread):
                 try:
                     sensor = sensor_pb2.Sensor()
                     sensor.ParseFromString(message)
-                    print (sensor)
+                    #print (sensor)
                     self._queue.put(sensor)
                 except Exception as e:
                     print "Error in Sensor parsing"
